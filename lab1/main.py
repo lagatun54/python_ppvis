77c1d3165689a95e3f821c8b3c2c846b2afe8503
lagatun54 <adidak2003@gmail.com>
2022-04-02 17:59:09 +0300

    refactor project

--- lab1/main.py
+++ lab1/main.py
@@ -1,50 +1,46 @@
 import os
+import sys
+import numpy as np
+import json
+import click
 from valley import *
 
+@click.command()
+@click.option('--new',is_flag=True, help='Start New Game')
+@click.option('--move',is_flag=True, help='Next step')
+@click.option('--print',is_flag=True, help="Print field and warehouse")
+@click.option('--plant', default='', help="plant garden")
+@click.option('--weed', default='', help="weed plant")
+@click.option('--water', default='', help="water plant")
+
+def main(new, move, print, plant, weed, water):
+    player = GameMaster()
+    if new:
+        player.storage.nullify_warehouse()
+        player.nullify_field()
+        player.export_plants()
+    else:
+        player.import_plants()
+        player.storage.display_warehouse()
+        if print:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            player.update_screen()
+        if move:
+            Events.start_disasters(player)
+            player.age_all()
+        if plant:
+            if int(plant) <= 7 and int(plant) > 0:
+                player.add_plant_based_on_id(int(plant))
+            player.age_all()
+        if weed:
+            if int(weed) <= 7 and int(weed) > 0:
+                player.weeding(int(weed))
+            player.age_all()
+            Events.start_disasters(player)
+        if water:
+            if int(water) <= 7 and int(water) > 0:
+                player.watering(water)
+            Events.start_disasters(player)
 
 if __name__ == '__main__':
-    player = GameMaster()
-
-    while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        player.update_screen()
-        Events.start_disasters(player)
-        print("\n\n\n1 - высадка растений\n2 - поливка растений\n3 - прополка грядок")
-        step = input()
-        match step:
-            case '':
-                player.age_all()
-            case '1':
-                player.planting()
-                player.age_all()
-            case '2':
-                player.watering()
-                player.age_all()
-            case '3':
-                player.weeding()
-                player.age_all()
-            case _:
-                exit()
-
-# прополка
-
-# события это класс, в котором есть флажки и методы
-# методы понижают модификатор каждого растения
-# события имеют рандомную продолжительность, после которой они заканчиваются и модификаторы возвращаются в норму
-# поливать можно отдельные грядки, это снимет их флажок и восстановит модификатор
-# по завершению события метод проходит по всем растениям и повышает модификатор у тех, у кого остался флажок
-
-
-"""
-Статика существует не в контексте объекта, а в контексте класса! 
-Из этого вытекает то, что на протяжении всего жизненного цикла вашего кода 
-будет существовать лишь одно глобальное состояние статических членов класса.
-
-Использовать статику нужно в случае, если то, что вы ей описываете принадлежит 
-всей группе объектов, а не одному. Например, у класса Human может быть статический 
-метод numberOfLegs(), который возвращает количество ног у людей. Количество ног - 
-это общее свойство для всех людей (Речь идет о здоровых людях). В данном случае 
-можно было использовать константу класса, но это не так важно, ведь, по сути, 
-константа - это тоже статический контекст. А вот имя - это уже свойство каждого 
-отдельного человека. 
-"""
+    main()
--- lab1/valley/game.py
+++ lab1/valley/game.py
@@ -3,18 +3,17 @@ from .gardenbed import GardenBed
 import numpy as np
 import random
 import json
+import os
 
 
 class GameMaster(Warehouse, GardenBed):
     field = GardenBed()
     storage = Warehouse()
-    with open('sw_templates.json', 'r', encoding='utf-8') as f:
-        house = json.load(f)
 
-    def watering(self):
-        number = int(input("Введите номер грядки: ")) - 1
-        if number > len(self.field.plants) or number < 0:
+    def watering(self, number):
+        if int(number) > len(self.field.plants) or int(number) < 0:
             return
+        number = int(number) - 1
         if self.field.plants[number].is_droughted:
             self.field.plants[number].is_droughted = False
             self.field.plants[number].mods += 0.5
@@ -25,46 +24,37 @@ class GameMaster(Warehouse, GardenBed):
     def age_all(self):  # Увеличивается процесс урожая ход
         for x in self.field.plants:
             x.age(self)
+        self.export_plants()
 
     def add_plant(self, plant_name):
         self.field.plants.append(plant_name)
 
     def add_plant_based_on_id(self, id_name):
-        match id_name:
-            case 0:
-                self.add_plant(Apple())
-            case 1:
-                self.add_plant(Pear())
-            case 2:
-                self.add_plant(Cherry())
-            case 3:
-                self.add_plant(Plum())
-            case 4:
-                self.add_plant(Potato())
-            case 5:
-                self.add_plant(Carrot())
-            case 6:
-                self.add_plant(Cabbage())
-            case 7:
-                self.add_plant(Pepper())
+        if id_name == 0:
+            self.add_plant(Apple())
+        if id_name == 1:
+            self.add_plant(Pear())
+        if id_name == 2:
+            self.add_plant(Cherry())
+        if id_name == 3:
+            self.add_plant(Plum())
+        if id_name == 4:
+            self.add_plant(Potato())
+        if id_name == 5:
+            self.add_plant(Carrot())
+        if id_name == 6:
+            self.add_plant(Cabbage())
+        if id_name == 7:
+            self.add_plant(Pepper())
 
     def display_garden(self):
         for i in self.field.plants:
             i.show_plant_status(self)
 
-    def planting(self):
-        number = int(input("Введите номер растения, которое хотите высадить:\n1 - яблоня\n2 - груша"
-                       "\n3 - вишня\n4 - слива\n5 - картофель\n6 - морковь\n7 - капуста\n8 - перец\n\n")) - 1
-        if number > 7 or number < 0:
-           return
-        #exception
-        else:
-            self.add_plant_based_on_id(number)
-
-    def weeding(self):
-        number = int(input("Введите номер грядки, которую хотите прополоть: ")) - 1
+    def weeding(self, number):
         if number > len(self.field.plants) or number < 0:
             return
+        number = int(number) - 1
         self.field.plants[number].weeded = False
         self.field.plants[number].mods += 0.2
         self.field.plants[number].mods = np.around(self.field.plants[number].mods, 3)
@@ -78,6 +68,54 @@ class GameMaster(Warehouse, GardenBed):
         self.storage.display_warehouse()
         print()
 
+    def import_plants(self):
+        house1 = {}
+        try:
+            working_directory = os.getcwd()
+            file_path = working_directory + '/field.json'
+
+            with open(file_path, 'r', encoding='utf-8') as f:
+                house1 = json.loads(f.read())
+                f.close()
+        except OSError:
+            print("Не удалось открыть файл!")
+            return
+        self.field.plants.clear()
+        for i in range(0, len(house1)):
+            current_plant: dict = house1.get(str(i))
+            self.add_plant_based_on_id(current_plant.get("id"))
+            self.field.plants[i].mods = current_plant.get("mods")
+            self.field.plants[i].harvest_progress = current_plant.get("harvest_progress")
+            self.field.plants[i].growth_progress = current_plant.get("growth_progress")
+            self.field.plants[i].id = current_plant.get("id")
+            self.field.plants[i].is_droughted = current_plant.get("is_droughted")
+            self.field.plants[i].weeded = current_plant.get("weeded")
+            self.field.plants[i].diseases = current_plant.get("diseases")
+            self.field.plants[i].has_colorado_beatle = current_plant.get("has_colorado_beatle")
+
+    def export_plants(self):
+        gamefield = dict()
+        for i in range(0, len(self.field.plants)):
+            current_plant = dict()
+            current_plant["id"] = self.field.plants[i].id
+            current_plant["mods"] = self.field.plants[i].mods
+            current_plant["harvest_progress"] = self.field.plants[i].harvest_progress
+            current_plant["growth_progress"] = self.field.plants[i].growth_progress
+            current_plant["is_droughted"] = self.field.plants[i].is_droughted
+            current_plant["weeded"] = self.field.plants[i].weeded
+            current_plant["diseases"] = self.field.plants[i].diseases
+            current_plant["has_colorado_beatle"] = self.field.plants[i].has_colorado_beatle
+            gamefield[str(i)] = current_plant
+        working_directory = os.getcwd()
+        file_path = working_directory + '/field.json'
+
+        with open(file_path, 'w', encoding='utf-8') as f:
+            json.dump(gamefield, f, ensure_ascii=False, indent=2)
+            f.close()
+
+    def nullify_field(self):
+        self.field.plants.clear()
+
 
 class Plant:  # Базовый класс
         harvest_progress: int = 0
@@ -89,7 +127,8 @@ class Plant:  # Базовый класс
         diseases: bool = False
         weeded: bool = False
         id: None
-        plods: str ='Plods'
+        plods: str = 'Plods'
+
 
 class Tree(Plant, GameMaster):  # Класс дерева
     growth_progress = 0
@@ -101,7 +140,7 @@ class Tree(Plant, GameMaster):  # Класс дерева
                 print("\n" + str(x + 1) + ": ", sep='', end='')
 
         if self.growth_progress < self.growth_max:
-            print(self.name , ". Рост дерева: " + str(self.growth_progress) + "/" + str(self.growth_max), sep='',
+            print(self.name, ". Рост дерева: " + str(self.growth_progress) + "/" + str(self.growth_max), sep='',
                   end='')
         else:
             print(self.name, ". Урожай: " + str(self.harvest_progress) + "/" + str(self.harvest_max) + " (M " +
@@ -119,9 +158,16 @@ class Tree(Plant, GameMaster):  # Класс дерева
                 if self.harvest_progress < self.harvest_max:
                     self.harvest_progress += 1
                 if self.harvest_progress == self.harvest_max:
-                    self.harvest_progress = 0
                     if random.random() < self.mods:
-                        target.storage.house[self.plods] += 1
+                        working_directory = os.getcwd()
+                        file_path = working_directory + '/warehouse.json'
+                        with open('file_path', 'r', encoding='utf-8') as f:
+                            house = json.loads(f.read())
+                        self.harvest_progress = 0
+                        self.storage.house[self.plods] += 1
+                        with open(file_path, 'w', encoding='utf-8') as f:
+                            json.dump(self.house, f, ensure_ascii=False, indent=2)
+
 
 
 class Vegetable(Plant, GameMaster):  # Класс овощей
@@ -129,7 +175,7 @@ class Vegetable(Plant, GameMaster):  # Класс овощей
         for x in range(0, len(target.field.plants)):
             if target.field.plants[x] == self:
                 print("\n" + str(x + 1) + ": ", sep='', end='')
-        print(self.name , ". Урожай: " + str(self.harvest_progress) + "/" + str(self.harvest_max) + " (M " +
+        print(self.name, ". Урожай: " + str(self.harvest_progress) + "/" + str(self.harvest_max) + " (M " +
               str(self.mods * 100) + "%)", sep='', end='')
         if self.weeded:
             print(" | ЗАХВАЧЕНО СОРНЯКАМИ", sep='', end='')
@@ -140,7 +186,14 @@ class Vegetable(Plant, GameMaster):  # Класс овощей
         if self.harvest_progress == self.harvest_max:
             self.harvest_progress = 0
             if random.random() < self.mods:
-                target.storage.house[self.plods] += 1
+                    working_directory = os.getcwd()
+                    file_path = working_directory + '/warehouse.json'
+                    with open('file_path', 'r', encoding='utf-8') as f:
+                        house = json.loads(f.read())
+                    self.harvest_progress = 0
+                    self.storage.house[self.plods] += 1
+                    with open(file_path, 'w', encoding='utf-8') as f:
+                        json.dump(self.house, f, ensure_ascii=False, indent=2)
 
 
 # FRUIT TREES
